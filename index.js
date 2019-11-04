42727261dd3f3e2754471f5bc240795da6e63dd1
truongle <truong.leduc194@gmail.com>
2019-11-04 10:04:06 +0700

    init project

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
+
+//view engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(3000, () => {
+    console.log('connect with port 3000');
+})
