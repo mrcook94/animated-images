3ffd51dd891b7cc71b14a99b0c7b9653c3829bfe
truongle <truong.leduc194@gmail.com>
2019-11-04 10:45:05 +0700

    updated

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const app = express()
 const path = require('path')
 const cors = require('cors')
+
+const app = express()
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')))
@@ -10,11 +11,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-
 app.use('/', (req, res) => {
     res.render('index')
 })
 
-app.listen(3000, () => {
+var port = process.env.PORT || 5000;
+
+app.listen(port, function () {
     console.log('connect with port 3000');
 })
